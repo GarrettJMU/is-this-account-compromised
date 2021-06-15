792b2eb72bc25f9d6847af4d756ca14cccff1a09
Garrett Hughes <garretthughes3@gmail.com>
2021-06-14 22:58:49 -0400

    add in upvote and downvote tech

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,18 @@
 class VotesController < ApplicationController
-  def create
+  def upvote
     session[:voting_ip] = request.remote_ip
     voter = User.find_or_create_by(ip: session[:voting_ip])
-    voter.likes @thread
+    account = Account.find params[:account_id]
+    account.liked_by voter
+    redirect_back(fallback_location: root_path)
+
+  end
+
+  def downvote
+    session[:voting_ip] = request.remote_ip
+    voter = User.find_or_create_by(ip: session[:voting_ip])
+    account = Account.find params[:account_id]
+    account.downvote_from voter
+    redirect_back(fallback_location: root_path)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :accounts, only: [:index, :create, :show, :new]
   root 'accounts#index'
   get '/status', to: 'internals#status'
+  get '/upvote/:account_id', to: 'votes#upvote'
+  get '/downvote/:account_id', to: 'votes#downvote'
 end
